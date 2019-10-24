fcd8cdfbb3a6103e2dfbdb39b704a0e98f959d90
MishaNavrotsky <MIsha172XDD@gmail.com>
2019-10-24 10:28:09 +0300

    f

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-window.onload = ()=>{
+window.onload = () => {
+    const dataInput = document.getElementById("dataInput"),
+        dataOutput = document.getElementById("dataOutput"),
+        popup = document.getElementById("popup");
+
+    dataOutput.addEventListener("click",()=>{
+        navigator.clipboard.writeText(dataOutput.value).then(()=>{
+            popup.style.display = "block";
+            setTimeout(()=>{
+                popup.style.display = "none";
+            }, 1000)
+        })
+    })
+
     
+
 }
